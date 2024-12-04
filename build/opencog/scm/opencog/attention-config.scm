a550a8ff243483ecd29ee21b0d0cb90a7333e19e
Nardos24 <narditatek@gmail.com>
2024-12-04 15:54:48 +0100

    first commit

---
+++ build/opencog/scm/opencog/attention-config.scm
@@ -0,0 +1,3 @@
+(define-module (opencog attention-config))
+(define-public opencog-ext-path-attentionbank "/home/nardos_tatek/attention/build/opencog/attentionbank/bank/")
+(define-public opencog-ext-path-attention-types "/home/nardos_tatek/attention/build/opencog/attentionbank/types/")
